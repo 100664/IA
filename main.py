7f92dd1c52f11bba3158cd591ec31b916d83516c
100664 <116294262+100664@users.noreply.github.com>
2023-11-06 17:58:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,69 @@ import datetime
 import time
 
 from extras import *
+from build import *
+
+def menu_mapa():
+    print()
+
+def menu_info():
+    print ("-----------------------------Proura informada----------------------------")
+    print ("|1 -> Procura A*                                                        |")
+    print ("|2 -> Proura Gulosa                                                     |")
+    print ("|3 -> Sair                                                              |")
+    print("--------------------------------------------------------------------------")
+
+    input1 = int (input())
+
+    if input1 == 1:
+        #procura A*
+        print("a*")
+        
+    elif input1 == 2:
+        #procura gulosa
+        print("greedy")
+    else:
+        menu_procura()
+
+        menu()
+
+def menu_ninfo():
+    print ("---------------------------Proura não informada--------------------------")
+    print ("|1 -> Procura em Profundidade                                           |")
+    print ("|2 -> Proura em Largura                                                 |")
+    print ("|3 -> Sair                                                              |")
+    print("--------------------------------------------------------------------------")
+
+    input1 = int (input())
+
+    if input1 == 1:
+        #procura em profundidade
+        print("prof")
+        
+    elif input1 == 2:
+        #procura em largura
+        print("larg")
+    else:
+        menu_procura()
+
+
+def menu_procura():
+    print ("--------------------------Proura do melhor Path--------------------------")
+    print ("|1 -> Procura não informada                                             |")
+    print ("|2 -> Procura informada                                                 |")
+    print ("|3 -> Sair                                                              |")
+    print("--------------------------------------------------------------------------")
+
+    
+    input1 = int(input())
+
+    if input1 == 1:
+        menu_ninfo()
+    elif input1 == 2:
+        menu_info()
+    else:
+        menu()
+
 
 
 def menu_preco(peso, tempo):
@@ -10,6 +73,7 @@ def menu_preco(peso, tempo):
     time.sleep(2)
     print("|        A sua encomenda ficará a " + encomenda_price + "€ .                                  |")
  
+    menu_procura()
 
 
 def menu_encomendas ():
@@ -34,17 +98,29 @@ def menu_encomendas ():
 
 def menu():
 
+#pode ser que o mapa tenha que ser criado pelo utilizador não sei bem como funciona a geração do mapa, por enquanto é um mapa pre-feito
+
     print("------------------------------Menu Principal------------------------------")
-    print("|1->Fazer uma encomenda                                                  |")
-    print("|2->Sair                                                                 |")
+    print("|1 -> Ver os Mapas                                                       |")
+    print("|2 -> Ver o mapa em forma de grafo                                       |")
+    print("|3 -> Fazer uma encomenda                                                |")
+    print("|4 -> Ver o que acontee quando há concorrência e choques                 |") #implementar em último, comecemos pelo básico
+    print("|4 - >Sair                                                               |")
     print("--------------------------------------------------------------------------")
     opcao = int(input())
 
-
     if opcao == 1:
-        menu_encomendas()
+        menu_mapa()
 
     elif opcao == 2:
+        print ("2")
+        #g = Grafo
+        #g.desenha()
+
+    elif opcao == 3:
+        menu_encomendas()
+
+    elif opcao ==4:
 
         return
 
