e23ede8e04e3deec86d2fa0480e47bbf319529c6
100664 <116294262+100664@users.noreply.github.com>
2023-11-06 19:10:49 +0000

    Update build.py

--- build.py
+++ build.py
@@ -9,4 +9,8 @@ class Graph:
         #self.h = {}
         self.helper = Helper(path)
         encomenda_pi = self.helper.get_encomenda_pi()
-        fst_node = Node(0, encomenda_pi[0], encomenda_pi[1])
+        node1 = Node(0, encomenda_pi[0], encomenda_pi[1]) # O node tem o 0 pois a posição de onde parte a encomenda ficará no grafo com o indice 0
+        self.nodes.append(node1) #coloco o nodolo de onde parte a encomeda na lista de nodulos
+        self.graph[0]=list() #o graph[0] é uma lista vazias pois o nodo1 de id = 0 nao tem "conexões"
+        
+
