5fb23aad7ebae5238f8ab0f71ebfd28fb52418c6
100664 <116294262+100664@users.noreply.github.com>
2023-11-07 19:53:37 +0000

    Update Helper.py

--- Helper.py
+++ Helper.py
@@ -1,12 +1,9 @@
 from parsezim import parse
 from Node import *
 
-from colorama import Fore, Back, Style
-
-class Helper: 
-
-    def __init__ (self, path):
-        self.path = path    
+class Helper:
+    def __init__(self, path):
+        self.path = path
         self.map = parse(path)
 
     def get_encomenda_pi(self):
@@ -16,5 +13,23 @@ class Helper:
             y = -1
             for pos in self.map[x]:
                 y += 1
-                if (pos == 'E'):
-                    return [y,x]
+                if pos == 'E':
+                    return [y, x]
+
+    def add_connection(self, node1, node2):
+        # Adicione uma conexão entre node1 e node2
+        if node1 and node2:
+            node1.add_connection(node2)
+            node2.add_connection(node1)
+
+    def is_valid_position(self, x, y):
+        # Verifique se a posição (x, y) é uma posição válida no mapa
+        if 0 <= y < len(self.map) and 0 <= x < len(self.map[y]):
+            return self.map[y][x] != 'X'  # Não é uma parede
+        return False
+
+    def is_destination(self, x, y):
+        # Verifique se a posição (x, y) é um destino (número de 0 a 9)
+        if 0 <= y < len(self.map) and 0 <= x < len(self.map[y]):
+            return '0' <= self.map[y][x] <= '9'
+        return False
