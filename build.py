4e69a9ecb2e62057fb01096de221c3d35047b087
Martim Redondo <116294262+100664@users.noreply.github.com>
2023-11-12 04:04:32 +0000

    Update build.py

--- build.py
+++ build.py
@@ -2,41 +2,46 @@ from Helper import *
 from Node import *
 
 import math
-from queue import Queue
+from queue import Queue, LifoQueue
 
-import networkx as nx  # biblioteca de tratamento de grafos necessária para desnhar graficamente o grafo
-import matplotlib.pyplot as plt  # ide
+import networkx as nx
+import matplotlib.pyplot as plt
 
 class Build:
-
-    def __init__ (self, path):
-
+    def __init__(self, path):
         self.nodes = []
         self.graph = {}
-        #self.h = {}
+        self.coordinates_to_node = {}
         self.helper = Helper(path)
-        encomenda_pi = self.helper.get_encomenda_pi()
-        node1 = Node(0, encomenda_pi[0], encomenda_pi[1]) # O node tem o 0 pois a posição de onde parte a encomenda ficará no grafo com o indice 0
-        self.nodes.append(node1) #coloco o nodolo de onde parte a encomeda na lista de nodulos
-        self.graph[0]= [] #o graph[0] é uma lista vazias pois o nodo1 de id = 0 nao tem "conexões"
+    
 
     def expand_graph(self):
-        # Passo 1: Encontre as coordenadas da letra "E"
         e_x, e_y = self.helper.get_encomenda_pi()
-
-        # Passo 2: Crie um nó com ID 0 nas coordenadas da letra "E"
         node_e = Node(0, e_x, e_y)
-        #self.nodes.append(node_e)
+        self.nodes.append(node_e)
         self.graph[0] = []
+        self.coordinates_to_node[(e_x, e_y)] = node_e
 
-        # Passo 3 e 4: Percorra as linhas da lista de strings
-        # para criar nós para caminhos "-" e conectá-los
         for y, line in enumerate(self.helper.map):
             for x, symbol in enumerate(line):
-                if symbol == 'E':
-                   break
                 if symbol == '-':
-                    node_id = len(self.nodes)+9  # Novo ID único
+                    # Verifique se um nodo com as mesmas coordenadas já existe
+                    coordinates = (x, y)
+                    new_node = self.coordinates_to_node.get(coordinates)
+
+                    if new_node is None:
+                        node_id = len(self.nodes) + 10
+                        new_node = Node(node_id, x, y)
+                        self.nodes.append(new_node)
+                        self.graph[node_id] = []
+                        self.coordinates_to_node[coordinates] = new_node
+
+                    # Conecte o novo nó aos nós vizinhos (caminhos possíveis)
+                    self.connect_nodes(new_node, x, y)
+
+                elif symbol.isdigit() and 1 <= int(symbol) <= 9:
+                    # Crie nós para destinos (números 1 a 9)
+                    node_id = int(symbol)
                     new_node = Node(node_id, x, y)
                     self.nodes.append(new_node)
                     self.graph[node_id] = []
@@ -44,19 +49,105 @@ class Build:
                     # Conecte o novo nó aos nós vizinhos (caminhos possíveis)
                     self.connect_nodes(new_node, x, y)
 
-                elif symbol.isdigit() and 1 <= int(symbol) <= 9:
-                    # Passo 5: Crie nós para destinos (números 1 a 9)
-                    node_id = int(symbol)
-                    new_node = Node(node_id, x, y)
-                    self.nodes.append(new_node)
-                    self.graph[node_id] = []
-
     def connect_nodes(self, node, x, y):
         # Conecte o nó aos nós vizinhos (caminhos possíveis)
-        possible_neighbors = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+        possible_neighbors = [
+        (x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1),
+        (x - 1, y - 1), (x + 1, y - 1), (x - 1, y + 1), (x + 1, y + 1)
+        ]
         for neighbor_x, neighbor_y in possible_neighbors:
-            for existing_node in self.nodes:
-                if (existing_node.x, existing_node.y) == (neighbor_x, neighbor_y):
-                    self.graph[node.node_id].append(existing_node.node_id)
-                    self.graph[existing_node.node_id].append(node.node_id)
+            coordinates = (neighbor_x, neighbor_y)
+            neighbor_node = self.coordinates_to_node.get(coordinates)
+
+            if neighbor_node:
+                # Conecte os nodos usando listas para evitar duplicatas
+                if neighbor_node.node_id not in self.graph[node.node_id]:
+                    self.graph[node.node_id].append(neighbor_node.node_id)
+                if node.node_id not in self.graph[neighbor_node.node_id]:
+                    self.graph[neighbor_node.node_id].append(node.node_id)
+
+
+
+
+    def visualize_graph(self, directed=False):
+        G = nx.Graph() if not directed else nx.DiGraph()
+
+        for node in self.nodes:
+            G.add_node(node.node_id, pos=(node.x, -node.y))  # Invertemos o eixo y para corresponder à representação do mapa
+
+        for node_id, connections in self.graph.items():
+            for neighbor_id in connections:
+                G.add_edge(node_id, neighbor_id)
+
+        pos = nx.get_node_attributes(G, 'pos')
+
+        labels = {node.node_id: node.node_id for node in self.nodes}
+
+        nx.draw(G, pos, with_labels=True, labels=labels, font_weight='bold', node_size=700, font_size=8, node_color='skyblue', edge_color='gray', arrowsize=20, connectionstyle='arc3,rad=0.1')
+
+        plt.show()
+
+    def bfs(self, destination_id):
+        start_node = self.nodes[0]
+        visited = set()
+        queue = Queue()
+        queue.put(start_node)
+
+        while not queue.empty():
+            current_node = queue.get()
+
+            if current_node.node_id == destination_id:
+                return self.reconstruct_path(start_node, current_node)
+
+            visited.add(current_node.node_id)
+
+            for neighbor_id in self.graph[current_node.node_id]:
+                neighbor = self.get_node_by_id(neighbor_id)
+
+                if neighbor.node_id not in visited and neighbor not in queue.queue:
+                    queue.put(neighbor)
+
+        return None  # Caminho não encontrado
+
+    def dfs(self, destination_id):
+        start_node = self.nodes[0]
+        visited = set()
+        stack = LifoQueue()
+        stack.put(start_node)
+
+        while not stack.empty():
+            current_node = stack.get()
+
+            if current_node.node_id == destination_id:
+                return self.reconstruct_path(start_node, current_node)
+
+            visited.add(current_node.node_id)
+
+            for neighbor_id in self.graph[current_node.node_id]:
+                neighbor = self.get_node_by_id(neighbor_id)
+
+                if neighbor.node_id not in visited and neighbor not in stack.queue:
+                    stack.put(neighbor)
+
+        return None  # Caminho não encontrado
+
+    def get_node_by_id(self, node_id):
+        for node in self.nodes:
+            if node.node_id == node_id:
+                return node
+        return None
+
+    def reconstruct_path(self, start_node, end_node):
+        path = [end_node.node_id]
+        current_node = end_node
+
+        while current_node.node_id != start_node.node_id:
+            for neighbor_id in self.graph[current_node.node_id]:
+                neighbor = self.get_node_by_id(neighbor_id)
+                if neighbor in path:
+                    current_node = neighbor
+                    path.insert(0, neighbor.node_id)
+                    break
+
+        return path
 
