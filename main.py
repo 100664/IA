069c78acf9b2235774b9164710fda56f7db96a3b
Martim Redondo <116294262+100664@users.noreply.github.com>
2023-11-12 17:20:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,23 @@ from Helper import *
 from build import *
 from extras import *
 
-def find_repeated_nodes(nodes):
+'''
+def find_repeated_nodes(nodles):
     node_count = {}
     repeated_nodes = []
 
-    for node in nodes:
+    for node in nodles:
         if node.node_id in node_count:
             repeated_nodes.append(node)
         else:
             node_count[node.node_id] = 1
 
     return repeated_nodes
+'''
+#-----------------------------//menu novo\\---------------------------------
 
-def menu_info():
+
+def menu_info(morada, caminho):
     print ("-----------------------------Proura informada----------------------------")
     print ("|1 -> Procura A*                                                        |")
     print ("|2 -> Proura Gulosa                                                     |")
@@ -37,9 +41,13 @@ def menu_info():
     else:
         menu_procura()
 
-        menu()
+    menu_principal()
 
-def menu_ninfo(morada):
+
+#-----------------------------//menu novo\\---------------------------------
+
+
+def menu_ninfo(morada, caminho):
     print ("---------------------------Proura não informada--------------------------")
     print ("|1 -> Procura em Profundidade                                           |")
     print ("|2 -> Proura em Largura                                                 |")
@@ -50,28 +58,27 @@ def menu_ninfo(morada):
 
     if input1 == 1:
         
-        builder = Build("freg.txt")
+        builder = Build(caminho)
         builder.expand_graph()
-        print("0")
-        destination_id = morada  # Substitua pelo ID de destino desejado
-        print(morada)
+        destination_id = morada
         bfs_path = builder.bfs(destination_id)
-        print ("1")
         print(f"Caminho BFS para o destino {destination_id}: {bfs_path}")
-        print("2")
         
     elif input1 == 2:
-        builder = Build("freg.txt")
+        builder = Build(menu_principal)
         builder.expand_graph()
-        destination_id = morada  # Substitua pelo ID de destino desejado
+        destination_id = morada
         dfs_path = builder.dfs(destination_id)
         print(f"Caminho DFS para o destino {destination_id}: {dfs_path}")
 
     else:
         menu_procura()
 
+    menu_principal
+#-----------------------------//menu novo\\---------------------------------
 
-def menu_procura(morada):
+
+def menu_procura(morada, caminho):
     print ("--------------------------Proura do melhor Path--------------------------")
     print ("|1 -> Procura não informada                                             |")
     print ("|2 -> Procura informada                                                 |")
@@ -82,15 +89,17 @@ def menu_procura(morada):
     input1 = int(input())
 
     if input1 == 1:
-        menu_ninfo(morada)
+        menu_ninfo(morada, caminho)
     elif input1 == 2:
-        menu_info()
+        menu_info(morada, caminho)
     else:
-        menu()
+        menu_principal()
+
+#-----------------------------//menu novo\\---------------------------------
 
 
 
-def menu_preco(peso, tempo, morada):
+def menu_preco(peso, tempo, morada, caminho):
     print("--------------------------------------------------------------------------")
     print("|     Aguarde, estamos a calcular quanto ficará a sua encomenda. . .     |")
     encomenda_price = str(calcular_preco_encomenda(peso, tempo))
@@ -98,10 +107,13 @@ def menu_preco(peso, tempo, morada):
     print("|        A sua encomenda ficará a " + encomenda_price + "€ .                                  |")
     print("---------------------------------------------------------------------------")
  
-    menu_procura(morada)
+    menu_procura(morada, caminho)
 
 
-def menu_encomendas ():
+#-----------------------------//menu novo\\---------------------------------
+
+
+def menu_encomendas (caminho):
     print("------------------------Encomendas e especificações-----------------------")
     print("|Em quanto tempo quer que a sua encomenda seja entregue (horas)          |") 
     print("|Qual o peso da encomenda em KG?                                         |")
@@ -121,50 +133,79 @@ def menu_encomendas ():
     print ("morada: ")
     morada = int (input())
 
-    menu_preco(peso, tempo, morada)
+    menu_preco(peso, tempo, morada, caminho)
 
 
+#-----------------------------//menu novo\\---------------------------------
 
-def menu():
 
-#pode ser que o mapa tenha que ser criado pelo utilizador não sei bem como funciona a geração do mapa, por enquanto é um mapa pre-feito
+def menu_principal (caminho):
 
     print("------------------------------Menu Principal------------------------------")
     print("|1 -> Ver Nodos e Conexões                                               |")
     print("|2 -> Ver o mapa em forma de grafo                                       |")
     print("|3 -> Fazer uma encomenda                                                |")
     print("|4 -> Ver o que acontece quando há concorrência e choques                |") #implementar em último, comecemos pelo básico
-    print("|4 - > Sair                                                              |")
+    print("|5 - > Sair                                                              |")
     print("--------------------------------------------------------------------------")
     opcao = int(input())
 
+
     if opcao == 1:
-                
-        builder = Build("/Users/martimr/Desktop/3ano1sem/IA/IA_projeto/Projeto_4/freguesia.txt")
+
+        builder = Build(caminho)
         builder.expand_graph()
-        nodes = builder.nodes
+
+        nodles = builder.nodes
         graph = builder.graph
-        for node in nodes:
+
+        for node in nodles:
             print(f"Node {node.node_id} - Coordenadas: ({node.x}, {node.y})")
-            print(f"Conexões: {list(graph[node.node_id])}")  # Converta o conjunto para lista para impressão
-        repeated_nodes = find_repeated_nodes(nodes)
+            print(f"Conexões: {list(graph[node.node_id])}")
+            '''
+        repeated_nodes = builder.find_repeated_nodes(nodles)
         if repeated_nodes:
             print("Nós repetidos:")
             for node in repeated_nodes:
                 print(f"Node {node.node_id} - Coordenadas: ({node.x}, {node.y})")
-        menu()
+            '''
+        menu_principal(caminho)
 
     elif opcao == 2:
-        builder = Build("/Users/martimr/Desktop/3ano1sem/IA/IA_projeto/Projeto_4/freguesia.txt")
+
+        builder = Build(caminho)
         builder.expand_graph()
         builder.visualize_graph()
-        menu()
+        menu_principal(caminho)
 
     elif opcao == 3:
-        menu_encomendas()
 
-    elif opcao ==4:
+        menu_encomendas(caminho)
 
+    elif opcao == 4:
+        print("ainda não está implementado")
+
+    elif opcao ==5:
+        menu()
+
+
+#-----------------------------//menu novo\\---------------------------------
+
+
+def menu():
+
+    print("-----------------------------------Menu-----------------------------------")
+    print("|1 -> Indique o caminho para o ficheiro no qual deseja trabalhar         |")
+    print("|2 -> Sair                                                               |")
+    print("--------------------------------------------------------------------------")
+    opcao = int(input())
+
+    if opcao == 1:
+        print("caminho: ")
+        caminho = input()
+        menu_principal(caminho)
+
+    elif opcao == 2:
         return
 
-menu() 
+menu()
